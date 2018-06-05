951842225bf91cadc7b79468baea9d5b111e0b6b
tkoar <terrancekoar@gmail.com>
2018-06-05 17:48:53 -0400

    updated and added a bar graph showing population by city for a given state

--- dashaltogethernow/dash_layout.py
+++ dashaltogethernow/dash_layout.py
@@ -12,48 +12,43 @@ from dash.dependencies import Input, Output
 from dashaltogethernow import app
 from dashaltogethernow.models import db, City, Demographic, State
 
-def generate_drop_down():
+def generate_pop_drop_down():
+    states = [{'label': state.name, 'value': state.name} for state in State.query.all()]
     return dcc.Dropdown(
-        id='sort-by-selector',
-        options=[
-            {'label': 'Country', 'value': 'Country'},
-            {'label': 'Pho', 'value': 'Pho'},
-            {'label': 'Ramen', 'value': 'Ramen'},
-            {'label': 'Soba', 'value': 'Soba'}
-        ],
-        value="Country"
+        id='state_selector',
+        options=states,
+        value="New Jersey"
     )
 
-def generate_scatter_plot():
-    all_cities = list(City.query.all())
-    traces = []
-    for city in all_cities[1:]:
-        traces.append(go.Scatter(x=city.demographics[0].population, y=city.demographics[0].mean_rent, text=city.state.name, mode='markers', opacity=0.7, name=city.state.name, marker={ 'size': 10, 'line': {'width': 0.5, 'color': 'blue'} }
-        ))
-    return dcc.Graph (
-        id='life-exp-vs-gdp',
-        figure={
-            'data': traces,
-            'layout': go.Layout(
-                xaxis={'title': 'City Population'},
-                yaxis={'title': 'City Mean Rent'},
-                hovermode='closest'
-            )
+def generate_bar_graphs(state="New Jersey"):
+    state_obj = State.query.filter(State.name == state).first()
+    all_cities = state_obj.cities
+    names = [city.name for city in all_cities]
+    populations = [city.demographics[0].population for city in all_cities]
+    return dcc.Graph(
+    id = "city_population_graph",
+    figure = {
+            'data': [{
+            'name': state_obj.name,
+            'x': names,
+            'y': populations,
+            'type': 'bar'
+            }],
+            'layout': {
+                'title': '{} Population Data'.format(state_obj.name)
+            }
         }
     )
 
 
 app.layout = html.Div(children=[
-    # html.Div(id='table_container'),
-    html.H3(children='Interest in Pho, Ramen, and Soba by Country according to Google Search from 01/2004 - 06/2018'),
-    generate_scatter_plot()
+    generate_pop_drop_down(),
+    html.Div(id='pop_graph_container'),
 ])
 
-# @app.callback(
-#     Output(component_id='table_container', component_property='children'),
-#     [Input(component_id='sort-by-selector', component_property='value')]
-# )
-# def sort_table(input_value):
-#     global data
-#     sorted_data = sorted(data, key=lambda datum: datum[input_value])
-#     return generate_table(sorted_data)
+@app.callback(
+    Output(component_id='pop_graph_container', component_property='children'),
+    [Input(component_id='state_selector', component_property='value')]
+)
+def change_table(input_value):
+    return generate_bar_graphs(input_value)
