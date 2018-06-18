8cf21a76184449158742a1774fb4258ce8c5b396
Jake <jakemacnaughton@gmail.com>
2018-06-18 13:28:57 -0400

    fixed make_city_object function

---
+++ test/test_test.py
@@ -0,0 +1,8 @@
+# import unittest, sys, sqlite3
+# sys.path.insert(0, '..')
+# from dashaltogethernow import db
+#
+# class TestDashAltogetherNow(unittest.TestCase):
+#     connection = sqlite3.connect(':app.db:')
+#     cursor = connection.cursor()
+#     import pdb; pdb.set_trace()
