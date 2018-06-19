f91e82577649f833d60f7b7a15723b3f07aa1113
Jake <jakemacnaughton@gmail.com>
2018-06-19 11:39:45 -0400

    removed solutions from updated master

--- run.py
+++
@@ -1,6 +0,0 @@
-from dashaltogethernow import app
-
-if __name__ == '__main__':
-    # adds in external css to style the table
-    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-    app.run_server(debug=True)
